8af86ec013963e046038fd79b341229af1a0ed4e
Aditya Atluri <Aditya.Atluri@amd.com>
2017-01-04 12:08:06 -0500

    added dst_unused functionality

--- include/shift/sdwa/declare_mov.h
+++ include/shift/sdwa/declare_mov.h
@@ -53,6 +53,23 @@ extern "C" int __shift_sdwa_mov_00_40_00_int(int);
 extern "C" int __shift_sdwa_mov_00_50_00_int(int);
 extern "C" int __shift_sdwa_mov_00_60_00_int(int);
 
+extern "C" int __shift_sdwa_mov_01_00_00_int(int);
+extern "C" int __shift_sdwa_mov_01_10_00_int(int);
+extern "C" int __shift_sdwa_mov_01_20_00_int(int);
+extern "C" int __shift_sdwa_mov_01_30_00_int(int);
+extern "C" int __shift_sdwa_mov_01_40_00_int(int);
+extern "C" int __shift_sdwa_mov_01_50_00_int(int);
+extern "C" int __shift_sdwa_mov_01_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_00_00_00_int(int);
+extern "C" int __shift_sdwa_mov_02_10_00_int(int);
+extern "C" int __shift_sdwa_mov_02_20_00_int(int);
+extern "C" int __shift_sdwa_mov_02_30_00_int(int);
+extern "C" int __shift_sdwa_mov_02_40_00_int(int);
+extern "C" int __shift_sdwa_mov_02_50_00_int(int);
+extern "C" int __shift_sdwa_mov_02_60_00_int(int);
+
+
 extern "C" int __shift_sdwa_mov_10_00_00_int(int);
 extern "C" int __shift_sdwa_mov_10_10_00_int(int);
 extern "C" int __shift_sdwa_mov_10_20_00_int(int);
@@ -60,6 +77,23 @@ extern "C" int __shift_sdwa_mov_10_30_00_int(int);
 extern "C" int __shift_sdwa_mov_10_40_00_int(int);
 extern "C" int __shift_sdwa_mov_10_50_00_int(int);
 extern "C" int __shift_sdwa_mov_10_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_11_00_00_int(int);
+extern "C" int __shift_sdwa_mov_11_10_00_int(int);
+extern "C" int __shift_sdwa_mov_11_20_00_int(int);
+extern "C" int __shift_sdwa_mov_11_30_00_int(int);
+extern "C" int __shift_sdwa_mov_11_40_00_int(int);
+extern "C" int __shift_sdwa_mov_11_50_00_int(int);
+extern "C" int __shift_sdwa_mov_11_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_12_00_00_int(int);
+extern "C" int __shift_sdwa_mov_12_10_00_int(int);
+extern "C" int __shift_sdwa_mov_12_20_00_int(int);
+extern "C" int __shift_sdwa_mov_12_30_00_int(int);
+extern "C" int __shift_sdwa_mov_12_40_00_int(int);
+extern "C" int __shift_sdwa_mov_12_50_00_int(int);
+extern "C" int __shift_sdwa_mov_12_60_00_int(int);
+
 
 extern "C" int __shift_sdwa_mov_20_00_00_int(int);
 extern "C" int __shift_sdwa_mov_20_10_00_int(int);
@@ -69,6 +103,23 @@ extern "C" int __shift_sdwa_mov_20_40_00_int(int);
 extern "C" int __shift_sdwa_mov_20_50_00_int(int);
 extern "C" int __shift_sdwa_mov_20_60_00_int(int);
 
+extern "C" int __shift_sdwa_mov_21_00_00_int(int);
+extern "C" int __shift_sdwa_mov_21_10_00_int(int);
+extern "C" int __shift_sdwa_mov_21_20_00_int(int);
+extern "C" int __shift_sdwa_mov_21_30_00_int(int);
+extern "C" int __shift_sdwa_mov_21_40_00_int(int);
+extern "C" int __shift_sdwa_mov_21_50_00_int(int);
+extern "C" int __shift_sdwa_mov_21_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_22_00_00_int(int);
+extern "C" int __shift_sdwa_mov_22_10_00_int(int);
+extern "C" int __shift_sdwa_mov_22_20_00_int(int);
+extern "C" int __shift_sdwa_mov_22_30_00_int(int);
+extern "C" int __shift_sdwa_mov_22_40_00_int(int);
+extern "C" int __shift_sdwa_mov_22_50_00_int(int);
+extern "C" int __shift_sdwa_mov_22_60_00_int(int);
+
+
 extern "C" int __shift_sdwa_mov_30_00_00_int(int);
 extern "C" int __shift_sdwa_mov_30_10_00_int(int);
 extern "C" int __shift_sdwa_mov_30_20_00_int(int);
@@ -76,6 +127,23 @@ extern "C" int __shift_sdwa_mov_30_30_00_int(int);
 extern "C" int __shift_sdwa_mov_30_40_00_int(int);
 extern "C" int __shift_sdwa_mov_30_50_00_int(int);
 extern "C" int __shift_sdwa_mov_30_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_31_00_00_int(int);
+extern "C" int __shift_sdwa_mov_31_10_00_int(int);
+extern "C" int __shift_sdwa_mov_31_20_00_int(int);
+extern "C" int __shift_sdwa_mov_31_30_00_int(int);
+extern "C" int __shift_sdwa_mov_31_40_00_int(int);
+extern "C" int __shift_sdwa_mov_31_50_00_int(int);
+extern "C" int __shift_sdwa_mov_31_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_32_00_00_int(int);
+extern "C" int __shift_sdwa_mov_32_10_00_int(int);
+extern "C" int __shift_sdwa_mov_32_20_00_int(int);
+extern "C" int __shift_sdwa_mov_32_30_00_int(int);
+extern "C" int __shift_sdwa_mov_32_40_00_int(int);
+extern "C" int __shift_sdwa_mov_32_50_00_int(int);
+extern "C" int __shift_sdwa_mov_32_60_00_int(int);
+
 
 extern "C" int __shift_sdwa_mov_40_00_00_int(int);
 extern "C" int __shift_sdwa_mov_40_10_00_int(int);
@@ -85,6 +153,23 @@ extern "C" int __shift_sdwa_mov_40_40_00_int(int);
 extern "C" int __shift_sdwa_mov_40_50_00_int(int);
 extern "C" int __shift_sdwa_mov_40_60_00_int(int);
 
+extern "C" int __shift_sdwa_mov_41_00_00_int(int);
+extern "C" int __shift_sdwa_mov_41_10_00_int(int);
+extern "C" int __shift_sdwa_mov_41_20_00_int(int);
+extern "C" int __shift_sdwa_mov_41_30_00_int(int);
+extern "C" int __shift_sdwa_mov_41_40_00_int(int);
+extern "C" int __shift_sdwa_mov_41_50_00_int(int);
+extern "C" int __shift_sdwa_mov_41_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_42_00_00_int(int);
+extern "C" int __shift_sdwa_mov_42_10_00_int(int);
+extern "C" int __shift_sdwa_mov_42_20_00_int(int);
+extern "C" int __shift_sdwa_mov_42_30_00_int(int);
+extern "C" int __shift_sdwa_mov_42_40_00_int(int);
+extern "C" int __shift_sdwa_mov_42_50_00_int(int);
+extern "C" int __shift_sdwa_mov_42_60_00_int(int);
+
+
 extern "C" int __shift_sdwa_mov_50_00_00_int(int);
 extern "C" int __shift_sdwa_mov_50_10_00_int(int);
 extern "C" int __shift_sdwa_mov_50_20_00_int(int);
@@ -92,6 +177,23 @@ extern "C" int __shift_sdwa_mov_50_30_00_int(int);
 extern "C" int __shift_sdwa_mov_50_40_00_int(int);
 extern "C" int __shift_sdwa_mov_50_50_00_int(int);
 extern "C" int __shift_sdwa_mov_50_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_51_00_00_int(int);
+extern "C" int __shift_sdwa_mov_51_10_00_int(int);
+extern "C" int __shift_sdwa_mov_51_20_00_int(int);
+extern "C" int __shift_sdwa_mov_51_30_00_int(int);
+extern "C" int __shift_sdwa_mov_51_40_00_int(int);
+extern "C" int __shift_sdwa_mov_51_50_00_int(int);
+extern "C" int __shift_sdwa_mov_51_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_52_00_00_int(int);
+extern "C" int __shift_sdwa_mov_52_10_00_int(int);
+extern "C" int __shift_sdwa_mov_52_20_00_int(int);
+extern "C" int __shift_sdwa_mov_52_30_00_int(int);
+extern "C" int __shift_sdwa_mov_52_40_00_int(int);
+extern "C" int __shift_sdwa_mov_52_50_00_int(int);
+extern "C" int __shift_sdwa_mov_52_60_00_int(int);
+
 
 extern "C" int __shift_sdwa_mov_60_00_00_int(int);
 extern "C" int __shift_sdwa_mov_60_10_00_int(int);
@@ -101,6 +203,21 @@ extern "C" int __shift_sdwa_mov_60_40_00_int(int);
 extern "C" int __shift_sdwa_mov_60_50_00_int(int);
 extern "C" int __shift_sdwa_mov_60_60_00_int(int);
 
+extern "C" int __shift_sdwa_mov_61_00_00_int(int);
+extern "C" int __shift_sdwa_mov_61_10_00_int(int);
+extern "C" int __shift_sdwa_mov_61_20_00_int(int);
+extern "C" int __shift_sdwa_mov_61_30_00_int(int);
+extern "C" int __shift_sdwa_mov_61_40_00_int(int);
+extern "C" int __shift_sdwa_mov_61_50_00_int(int);
+extern "C" int __shift_sdwa_mov_61_60_00_int(int);
+
+extern "C" int __shift_sdwa_mov_62_00_00_int(int);
+extern "C" int __shift_sdwa_mov_62_10_00_int(int);
+extern "C" int __shift_sdwa_mov_62_20_00_int(int);
+extern "C" int __shift_sdwa_mov_62_30_00_int(int);
+extern "C" int __shift_sdwa_mov_62_40_00_int(int);
+extern "C" int __shift_sdwa_mov_62_50_00_int(int);
+extern "C" int __shift_sdwa_mov_62_60_00_int(int);
 
 
 #endif
